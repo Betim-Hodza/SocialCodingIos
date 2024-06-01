f7db36c01dcee59f051ea15886bdd012e80d7fab
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-06-01 18:28:38 -0500

    Initial Commit

---
+++ SocialCodingIos/SocialCodingIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialCodingIosApp.swift
+//  SocialCodingIos
+//
+//  Created by Betim Hodza on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialCodingIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
