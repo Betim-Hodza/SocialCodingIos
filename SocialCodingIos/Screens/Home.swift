0450859ef637a97827ae0cbab1b6ea34a6f8b89e
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-06-02 13:56:01 -0500

    Feat+Update: Home Screen
    Home Screen is almost complete, just missing a few ui features, but we got buttons that work!

--- SocialCodingIos/Screens/Home.swift
+++ SocialCodingIos/Screens/Home.swift
@@ -26,11 +26,125 @@ struct Home: View {
                 
             }
             
+            //MOBI LOGO
+            Image("MOBI_logo")
+                .imageScale(.small)
+                .clipShape(.circle)
+                .overlay(
+                    Circle()
+                        .stroke(Color.mobiBorder, lineWidth: 4)
+                )
+            
+            //MOBI Slanted Rectangle Tag
+            MOBIRec(title: "MOBI", tagline: "Disocver the magic of making apps!")
+            
+            //Become a member Rect button
+            MOBIRecBttn(url: "https://discord.com/invite/pjjgRC7ErT", text: "Become a MOBI Member! 🚀")
+            
+            //Social Media Buttons
+            
+            HStack {
+                //Youtube
+                MOBISocialsBttn(url: "https://youtube.com/@mobibyte", img: "Youtube")
+                //Instagram
+                MOBISocialsBttn(url: "https://www.instagram.com/codewithmobi", img: "Instagram")
+                //Github
+                MOBISocialsBttn(url: "https://www.github.com/mobibyte", img: "Github")
+                //FaceBook
+                MOBISocialsBttn(url: "https://www.facebook.com/codewithmobi", img: "Facebook")
+                //Outlook
+                MOBISocialsBttn(url: "contact@codewith.mobi", img: "Outlook")
+            }
+            .padding(.horizontal, 10)
+            
+            
+            
+        
+            //filler text
             Spacer()
             Text("Home")
             Spacer()
             
         }
         .padding(.horizontal, 24)
+    }
+}
+
+//MOBI Rectangle Tag
+struct MOBIRec: View {
+    
+    let title: String
+    let tagline: String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill( LinearGradient(
+                    colors: [.white, .mintBlue],
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ))
+                .stroke(Color.mobiBorder, lineWidth: 2)
+                .frame(width: 330, height: 150)
+            
+            VStack{
+                Text(title)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .padding(.bottom, 5)
+                
+                Text(tagline)
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                    .padding(.horizontal, 5)
+            }
+        }
+        .padding(.top, 15)
+    }
+}
+
+struct MOBIRecBttn: View {
+    
+    let url: String
+    let text: String
+    
+    var body: some View {
+        
+        Link(destination: URL(string: url)!) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill( LinearGradient(
+                        colors: [.lightDarkBlue, .lillypurple],
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    ))
+                    .stroke(Color.mobiBorder, lineWidth: 2)
+                    .frame(width: 330, height: 100)
+                
+                Text(text)
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
+
+struct MOBISocialsBttn: View {
+    
+    let url: String
+    let img: String
+    
+    var body: some View {
+        Link(destination: URL(string: url)!) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.white)
+                    .stroke(Color.mobiBorder, lineWidth: 2)
+                    .frame(width: 65, height: 65)
+                
+                Image(img)
+            }
+        }
     }
 }
