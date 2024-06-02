e2831017a30847038c7b5783f535c3b5cc960a3f
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-06-01 19:30:13 -0500

    Feat: Side nav menu
    Probably one of the more complicated ways ive seen a side menu nav done. heres the ref: https://medium.com/geekculture/side-menu-in-ios-swiftui-9fe1b69fc487

--- SocialCodingIos/ContentView.swift
+++ SocialCodingIos/ContentView.swift
@@ -8,16 +8,298 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var presentSideMenu = false
+    @State var selectedSideMenuTab = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            TabView(selection: $selectedSideMenuTab) {
+                HomeView(presentSideMenu: $presentSideMenu)
+                    .tag(0)
+                FavoriteView(presentSideMenu: $presentSideMenu)
+                    .tag(1)
+                ChatView(presentSideMenu: $presentSideMenu)
+                    .tag(2)
+                ProfileView(presentSideMenu: $presentSideMenu)
+                    .tag(3)
+            }
+            
+            SideMenu(isShowing: $presentSideMenu, content: AnyView(SideMenuView(selectedSideMenuTab: $selectedSideMenuTab, presentSideMenu: $presentSideMenu)))
         }
-        .padding()
     }
 }
+
+struct HomeView: View {
+    
+    @Binding var presentSideMenu: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    presentSideMenu.toggle()
+                } label: {
+                    Image("Menu")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                }
+                Spacer()
+                
+            }
+            
+            Spacer()
+            Text("Home View")
+            Spacer()
+            
+        }
+        .padding(.horizontal, 24)
+    }
+}
+
+struct FavoriteView: View {
+    
+    @Binding var presentSideMenu: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    presentSideMenu.toggle()
+                } label: {
+                    Image("Favorite")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                }
+                Spacer()
+                
+            }
+            
+            Spacer()
+            Text("Favorite View")
+            Spacer()
+            
+        }
+        .padding(.horizontal, 24)
+    }
+}
+
+struct ChatView: View {
+    
+    @Binding var presentSideMenu: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    presentSideMenu.toggle()
+                } label: {
+                    Image("Chat")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                }
+                Spacer()
+                
+            }
+            
+            Spacer()
+            Text("Chat View")
+            Spacer()
+            
+        }
+        .padding(.horizontal, 24)
+    }
+}
+
+struct ProfileView: View {
+    
+    @Binding var presentSideMenu: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    presentSideMenu.toggle()
+                } label: {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                }
+                Spacer()
+                
+            }
+            
+            Spacer()
+            Text("Profile View")
+            Spacer()
+            
+        }
+        .padding(.horizontal, 24)
+    }
+}
+
+struct SideMenu: View {
+    @Binding var isShowing: Bool
+    var content: AnyView
+    var edgeTransition: AnyTransition = .move(edge: .leading)
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            if (isShowing) {
+                Color.black
+                    .opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        isShowing.toggle()
+                    }
+                content
+                    .transition(edgeTransition)
+                    .background(
+                        Color.clear
+                    )
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
+        .animation(.easeInOut, value: isShowing)
+    }
+}
+
+enum SideMenuRowType: Int, CaseIterable{
+    case home = 0
+    case favorite
+    case chat
+    case profile
+    
+    var title: String {
+        switch self {
+        case .home:
+            return "home"
+        case .favorite:
+            return "favorite"
+        case .chat:
+            return "chat"
+        case .profile:
+            return "profile"
+        }
+    }
+    
+    var iconName: String{
+        switch self {
+        case .home:
+            return "Home"
+        case .favorite:
+            return "Favorite"
+        case .chat:
+            return "Chat"
+        case .profile:
+            return "person.circle.fill"
+        }
+    }
+}
+
+struct SideMenuView: View {
+    
+    @Binding var selectedSideMenuTab: Int
+    @Binding var presentSideMenu: Bool
+    
+    var body: some View {
+        HStack {
+            
+            ZStack {
+                Rectangle()
+                    .fill(.white)
+                    .frame(width: 270)
+                    .shadow(color: .purple.opacity(0.1), radius: 5, x: 0, y: 3)
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    ProfileImageView()
+                        .frame(height: 140)
+                        .padding(.bottom, 30)
+                    
+                    ForEach(SideMenuRowType.allCases, id: \.self) { row in
+                        RowView(isSelected: selectedSideMenuTab == row.rawValue, imageName: row.iconName, title: row.title) {
+                            selectedSideMenuTab = row.rawValue
+                            presentSideMenu.toggle()
+                        }
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top, 100)
+                .frame(width: 270)
+                .background(
+                    Color.white
+                )
+            }
+            
+            Spacer()
+        }
+        .background(.clear)
+    }
+    
+    func ProfileImageView() -> some View {
+        VStack(alignment: .center) {
+            HStack {
+                Spacer()
+                Image("profile-image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .overlay (
+                        RoundedRectangle (cornerRadius: 50)
+                            .stroke(.purple.opacity(0.5), lineWidth: 10)
+                    )
+                    .cornerRadius(50)
+                Spacer()
+            }
+            
+            Text("Baytizzel")
+                .font(.system(size: 18, weight: .bold))
+                .foregroundColor(.black)
+            
+            Text("Future IOS/Android Dev")
+                .font(.system(size: 14, weight: .semibold))
+                .foregroundColor(.black.opacity(0.5))
+            
+        }
+    }
+    
+    
+    func RowView(isSelected: Bool, imageName: String, title: String, hideDivider: Bool = false, action: @escaping (()->())) -> some View {
+        Button {
+            action()
+        } label: {
+            VStack (alignment: .leading) {
+                HStack(spacing: 20){
+                    Rectangle()
+                        .fill(isSelected ? .purple : .white)
+                        .frame(width: 5)
+                    
+                    ZStack{
+                        Image(imageName)
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundColor(isSelected ? .black : .gray)
+                            .frame(width: 26, height: 26)
+                    }
+                    .frame(width: 30, height: 30)
+                    Text(title)
+                        .font(.system(size: 14, weight: .regular))
+                        .foregroundColor(isSelected ? .black : .gray)
+                    Spacer()
+                }
+            }
+        }
+        .frame(height: 50)
+        .background(
+            LinearGradient(colors: [isSelected ? .purple.opacity(0.5) : .white, .white], startPoint: .leading, endPoint: .trailing)
+        )
+    }
+    
+}
+
 
 #Preview {
     ContentView()
